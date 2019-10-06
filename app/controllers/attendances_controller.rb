252ac6b51912471bb0b4e7dd628cab424f3f404d
斉藤庸一郎 <yosa1129@yahoo.co.jp>
2019-10-06 13:26:19 +0000

    11章11.1.3終了

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,10 +1,14 @@
 class AttendancesController < ApplicationController
+  before_action :set_user, only: :edit_one_month
+  before_action :logged_in_user, only: [:update, :edit_one_month]
+  before_action :set_one_month, only: :edit_one_month
+
   UPDATE_ERROR_MSG = "勤怠登録に失敗しました。やり直してください。"
-  
+
   def update
     @user = User.find(params[:user_id])
     @attendance = Attendance.find(params[:id])
-    #出勤時間が未登録であることを判定します。
+    # 出勤時間が未登録であることを判定します。
     if @attendance.started_at.nil?
       if @attendance.update_attributes(started_at: Time.current.change(sec: 0))
         flash[:info] = "おはようございます！"
@@ -13,11 +17,14 @@ class AttendancesController < ApplicationController
       end
     elsif @attendance.finished_at.nil?
       if @attendance.update_attributes(finished_at: Time.current.change(sec: 0))
-        flash[:info] = "お疲れ様でした"
+        flash[:info] = "お疲れ様でした。"
       else
         flash[:danger] = UPDATE_ERROR_MSG
       end
     end
     redirect_to @user
+  end
+
+  def edit_one_month
   end
 end
